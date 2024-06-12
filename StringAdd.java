d9aafa464c0ed8356f2e7cf1a46b858c4eb3c8be
ZiniuZheng <117133246+ZiniuZheng@users.noreply.github.com>
2024-06-12 17:45:12 +0800

    Create StringAdd.java

---
+++ StringAdd.java
@@ -0,0 +1,17 @@
+package com.demo;
+/**
+ * 字符串的加法
+ * @author zhengziniu
+ *
+ */
+public class StringAdd {
+
+	public static void main(String[] args) {
+		String a = "我";
+		String b = "是";
+		String c = "程序员";
+		System.out.println(a+b+c);
+
+	}
+
+}
