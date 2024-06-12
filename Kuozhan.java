bef5bd585912b29ceb6b754ecb9293060ab47f55
ZiniuZheng <117133246+ZiniuZheng@users.noreply.github.com>
2024-06-12 17:43:51 +0800

    Create Kuozhan.java

---
+++ Kuozhan.java
@@ -0,0 +1,18 @@
+package com.demo;
+/**
+ * 扩展运算符
+ * @author zhengziniu
+ *
+ */
+
+public class Kuozhan {
+
+	public static void main(String[] args) {
+		int i = 10;
+		int j = i + 1;
+		j %= 2;
+		System.out.println(j);
+
+	}
+
+}
