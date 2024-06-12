84b625e66727a4c7775bb609215c150f124aaf16
ZiniuZheng <117133246+ZiniuZheng@users.noreply.github.com>
2024-06-12 17:44:12 +0800

    Create Sanyuan.java

---
+++ Sanyuan.java
@@ -0,0 +1,18 @@
+package com.demo;
+
+public class Sanyuan {
+
+	public static void main(String[] args) {
+		int x = 3;
+		int y = 4;
+		int z;
+		if(x>y) {
+			z = x;
+		}else {
+			z = y;
+		}
+		System.out.println(z);
+
+	}
+
+}
