b6c31ca868a4a1162aff3e4665d86c951413264c
ZiniuZheng <117133246+ZiniuZheng@users.noreply.github.com>
2024-06-12 17:42:05 +0800

    Create CharDemo.java

---
+++ CharDemo.java
@@ -0,0 +1,16 @@
+package com.demo;
+/**
+ * 字符运算
+ * @author zhengziniu
+ *
+ */
+public class CharDemo {
+
+	public static void main(String[] args) {
+		char a = 'a';
+		char b = 'b';
+		System.out.println(a<=b);
+
+	}
+
+}
